47fa899a0f36c8fabe50632e0c485fbeeb539001
CodeNinja0101 <ghumre319@gmail.com>
2024-07-27 02:38:26 +0530

    consumer changes added

---
+++ kafka-consumer-example/src/main/java/com/example/kafka_consumer_example/KafkaConsumerExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafka_consumer_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaConsumerExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaConsumerExampleApplication.class, args);
+	}
+
+}
---
+++ kafka-consumer-example/src/test/java/com/example/kafka_consumer_example/KafkaConsumerExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafka_consumer_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaConsumerExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
