4e348ffcb7e7f4dc4158bfb3ddd597832df177bf
Shrikrushna Ghumre <167312445+CodeNinja0101@users.noreply.github.com>
2024-07-27 01:59:51 +0530

    Add files via upload
    producer functionality added

---
+++ kafka-producer-example/src/main/java/com/example/kafka_producer_example/KafkaProducerExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafka_producer_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaProducerExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaProducerExampleApplication.class, args);
+	}
+
+}
